2600280633252dddf3c4dadd86ddbb49123900c7
AurePay SDK Sync <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-27 16:19:04 +0000

    chore(sdk): sync from aurepay monorepo (2026-07-27T16:19:04.689Z)

--- lib/aurepay/generated/api/deposits_api.rb
+++ lib/aurepay/generated/api/deposits_api.rb
@@ -146,6 +146,69 @@ module AurePay
       return data, status_code, headers
     end
 
+    # Payload request/response do depósito
+    # Retorna o request e o response da chamada de API que criou o depósito. Retenção de 90 dias; após isso responde PAYLOAD_NOT_FOUND. 
+    # @param id [String] Identificador ULID do recurso
+    # @param [Hash] opts the optional parameters
+    # @return [SuccessEnvelope]
+    def deposits_get_payload(id, opts = {})
+      data, _status_code, _headers = deposits_get_payload_with_http_info(id, opts)
+      data
+    end
+
+    # Payload request/response do depósito
+    # Retorna o request e o response da chamada de API que criou o depósito. Retenção de 90 dias; após isso responde PAYLOAD_NOT_FOUND. 
+    # @param id [String] Identificador ULID do recurso
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(SuccessEnvelope, Integer, Hash)>] SuccessEnvelope data, response status code and response headers
+    def deposits_get_payload_with_http_info(id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: DepositsApi.deposits_get_payload ...'
+      end
+      # verify the required parameter 'id' is set
+      if @api_client.config.client_side_validation && id.nil?
+        fail ArgumentError, "Missing the required parameter 'id' when calling DepositsApi.deposits_get_payload"
+      end
+      # resource path
+      local_var_path = '/deposits/{id}/payload'.sub('{id}', CGI.escape(id.to_s))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json']) unless header_params['Accept']
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'SuccessEnvelope'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['ApiKeyAuth', 'ApiSecretAuth']
+
+      new_options = opts.merge(
+        :operation => :"DepositsApi.deposits_get_payload",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: DepositsApi#deposits_get_payload\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # Lista depósitos
     # @param [Hash] opts the optional parameters
     # @return [SuccessEnvelope]
--- lib/aurepay/generated/api/withdrawals_api.rb
+++ lib/aurepay/generated/api/withdrawals_api.rb
@@ -146,6 +146,69 @@ module AurePay
       return data, status_code, headers
     end
 
+    # Payload request/response do saque
+    # Retorna o request e o response da chamada de API que criou o saque. Retenção de 90 dias; após isso responde PAYLOAD_NOT_FOUND. 
+    # @param id [String] Identificador ULID do recurso
+    # @param [Hash] opts the optional parameters
+    # @return [SuccessEnvelope]
+    def withdrawals_get_payload(id, opts = {})
+      data, _status_code, _headers = withdrawals_get_payload_with_http_info(id, opts)
+      data
+    end
+
+    # Payload request/response do saque
+    # Retorna o request e o response da chamada de API que criou o saque. Retenção de 90 dias; após isso responde PAYLOAD_NOT_FOUND. 
+    # @param id [String] Identificador ULID do recurso
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(SuccessEnvelope, Integer, Hash)>] SuccessEnvelope data, response status code and response headers
+    def withdrawals_get_payload_with_http_info(id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: WithdrawalsApi.withdrawals_get_payload ...'
+      end
+      # verify the required parameter 'id' is set
+      if @api_client.config.client_side_validation && id.nil?
+        fail ArgumentError, "Missing the required parameter 'id' when calling WithdrawalsApi.withdrawals_get_payload"
+      end
+      # resource path
+      local_var_path = '/withdrawals/{id}/payload'.sub('{id}', CGI.escape(id.to_s))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json']) unless header_params['Accept']
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'SuccessEnvelope'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['ApiKeyAuth', 'ApiSecretAuth']
+
+      new_options = opts.merge(
+        :operation => :"WithdrawalsApi.withdrawals_get_payload",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: WithdrawalsApi#withdrawals_get_payload\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # Lista saques
     # @param [Hash] opts the optional parameters
     # @return [SuccessEnvelope]
